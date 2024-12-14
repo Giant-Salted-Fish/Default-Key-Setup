e8dd1c538e61e57e09957a48f14f4a901aaef74c
Giant_Salted_Fish <2052302742@qq.com>
2024-12-14 10:52:54 +0800

    Adapt 1.0.0.5 change

--- src/main/java/com/dks/client/DKSMod.java
+++ src/main/java/com/dks/client/DKSMod.java
@@ -37,10 +37,28 @@ public final class DKSMod
 {
 	private static void __saveDefaultKeySetup()
 	{
+		final var has_kbp_mod = ModList.get().isLoaded( "key_binding_patch" );
+		final BiConsumer< KeyMapping, StringBuilder > append_value;
+		if ( has_kbp_mod ) {
+			append_value = ( km, builder ) -> builder.append( km.saveString() );
+		}
+		else
+		{
+			append_value = ( km, builder ) -> {
+				builder.append( km.saveString() );
+				builder.append( ':' );
+				builder.append( km.getKeyModifier() );
+			};
+		}
 		final var mc = Minecraft.getInstance();
 		DKSModConfig.DEFAULT_KEY_SETUP.set(
 			Arrays.stream( mc.options.keyMappings )
-			.map( km -> String.format( "%s=%s", km.getName(), km.saveString() ) )
+			.map( km -> {
+				final StringBuilder builder = new StringBuilder( km.getName() );
+				builder.append( '=' );
+				append_value.accept( km, builder );
+				return builder.toString();
+			} )
 			.toList()
 		);
 		DKSModConfig.DEFAULT_KEY_SETUP.save();
@@ -49,47 +67,6 @@ public final class DKSMod
 	private static void __applyDefaultKeySetup()
 	{
 		final var has_kbp_mod = ModList.get().isLoaded( "key_binding_patch" );
-		final BiConsumer< KeyMapping, String[] > setup_rest;
-		if ( has_kbp_mod )
-		{
-			setup_rest = ( km, split ) -> {
-				final ImmutableSet< Key > cmb_keys;
-				final KeyModifier modifier;
-				if ( split.length == 1 )
-				{
-					cmb_keys = ImmutableSet.of();
-					modifier = KeyModifier.NONE;
-				}
-				else
-				{
-					modifier = KeyModifier.valueFromString( split[ 1 ] );
-					if ( split.length > 2 )
-					{
-						cmb_keys = (
-							Arrays.stream( split[ 2 ].split( "\\+" ) )
-							.map( InputConstants::getKey )
-							.collect( ImmutableSet.toImmutableSet() )
-						);
-					}
-					else {
-						cmb_keys = IKeyMappingImpl.toCmbKeySet( modifier );
-					}
-				}
-				
-				KeyMappingAccess._setKeyModifierDefault( km, modifier );
-				KeyMappingAccess._setDefaultCmbKeys( km, cmb_keys );
-			};
-		}
-		else
-		{
-			// KBP mod is not installed, so can not use IKeyMappingImpl#toModifier(...).
-			setup_rest = ( km, split ) -> {
-				final String value = split.length > 1 ? split[ 1 ] : "";
-				final var modifier = KeyModifier.valueFromString( value );
-				KeyMappingAccess._setKeyModifierDefault( km, modifier );
-			};
-		}
-		
 		final var data = (
 			DKSModConfig.DEFAULT_KEY_SETUP.get().stream()
 			.map( s -> {
@@ -109,8 +86,25 @@ public final class DKSMod
 			{
 				final var split = value.split( ":" );
 				final var key = InputConstants.getKey( split[ 0 ] );
+				final var modifier = KeyModifier.valueFromString( split[ 1 ] );
 				KeyMappingAccess._setDefaultKey( km, key );
-				setup_rest.accept( km, split );
+				KeyMappingAccess._setKeyModifierDefault( km, modifier );
+				if ( has_kbp_mod )
+				{
+					final ImmutableSet< Key > cmb_keys;
+					if ( split.length > 2 )
+					{
+						cmb_keys = (
+							Arrays.stream( split[ 2 ].split( "\\+" ) )
+							.map( InputConstants::getKey )
+							.collect( ImmutableSet.toImmutableSet() )
+						);
+					}
+					else {
+						cmb_keys = IKeyMappingImpl.toCmbKeySet( modifier );
+					}
+					KeyMappingAccess._setDefaultCmbKeys( km, cmb_keys );
+				}
 			}
 		}
 	}
